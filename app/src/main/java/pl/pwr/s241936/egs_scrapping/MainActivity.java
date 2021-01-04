f3dcee547a74a48918e634d7ffb32c6c2d80ebd3
M1ko5ek <mikolajlempart@gmail.com>
2021-01-04 21:28:52 +0100

    nie działa ale jakiś html z JS mam

--- app/src/main/java/pl/pwr/s241936/egs_scrapping/MainActivity.java
+++ app/src/main/java/pl/pwr/s241936/egs_scrapping/MainActivity.java
@@ -1,8 +1,18 @@
 package pl.pwr.s241936.egs_scrapping;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final WebView mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.addJavascriptInterface(new MyJavaScriptInterface(this), "HtmlViewer");
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mWebView.loadUrl("javascript:HtmlViewer.showHTML" +
+                        "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');");
+            }
+        });
+
+        mWebView.loadUrl("https://www.epicgames.com/store/pl/browse?pageSize=30&q=assassin%27s&sortBy=relevance&sortDir=DESC");
+    }
+
+    class MyJavaScriptInterface {
+
+        private Context ctx;
+
+        MyJavaScriptInterface(Context ctx) {
+            this.ctx = ctx;
+        }
+
+        @JavascriptInterface
+        public void showHTML(String html) {
+            System.out.println(html);
+        }
+
     }
 }
